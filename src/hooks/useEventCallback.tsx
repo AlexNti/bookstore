c61534c233d7df54a56441d1f4ebd7c02c8d43be
Alexandros <a.ntitoras@gmail.com>
2020-06-06 11:06:21 +0300

    added useForm and helper function for use form

---
+++ src/hooks/useEventCallback.tsx
@@ -0,0 +1,16 @@
+import {useCallback, useEffect, useRef} from 'react'
+
+
+const useEventCallback = <T extends (...args: any[]) => any>(fn: T) => {
+  const ref = useRef<T>(fn)
+
+  useEffect(() => {
+    ref.current = fn
+  }, [fn])
+
+  return useCallback((...args: any[]) => {
+    return ref.current(...args)
+  }, [])
+}
+
+export default useEventCallback
