6a817210b899143e701bdd6114f681f296645852
Alexandros <a.ntitoras@gmail.com>
2020-06-06 11:04:49 +0300

    Layout and global styles added

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+import Header from '../Header'
+
+const LayoutRoot = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  padding: '20px',
+  backgroundColor: 'white',
+})
+
+const Layout = ({children}: {children: React.ReactNode}): JSX.Element => (
+  <LayoutRoot>
+    <Header />
+    {children}
+  </LayoutRoot>
+)
+
+export default Layout
