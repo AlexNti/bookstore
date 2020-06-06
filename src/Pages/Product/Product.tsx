3756e8f6177e392f1a03378b273d5d48f1f6902f
Alexandros <a.ntitoras@gmail.com>
2020-06-06 11:11:56 +0300

    added flex to prodcut left and right page layout

--- src/Pages/Product/Product.tsx
+++ src/Pages/Product/Product.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
 import styled from '@emotion/styled'
+
+import ImageField from './components/ImageField'
 
 const ProductPageLayout = styled('div')({
   display: 'flex',
@@ -12,17 +13,21 @@ const LeftPageLayout = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   height: '100%',
+  flex: 1,
 })
 
 const RightPageLayout = styled('div')({
   display: 'flex',
   flexDirection: 'column',
   height: '100%',
+  flex: 1,
 })
 
 const Product = (): JSX.Element => (
   <ProductPageLayout>
-    <LeftPageLayout>Here is left</LeftPageLayout>
+    <LeftPageLayout>
+      <ImageField></ImageField>
+    </LeftPageLayout>
     <RightPageLayout>Here is right</RightPageLayout>
   </ProductPageLayout>
 )
